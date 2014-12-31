315ff185a56214b93aab27191506026fe476c508
user <user@HP-PRO>
2014-12-31 09:06:13 +0200

    Фіксація

---
+++ Hello/src/hello/Hello.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        print();
+	}
+    private static void print(){
+         System.out.println("Hello World!");
+    }
+
+}
